82177e062cb96f3663b8cfd06fa28748fee4230e
David Björklund <david.bjorklund@gmail.com>
2013-03-14 15:04:51 +0100

    [test] Refactor test, easier setup of tests

--- test/test.js
+++ test/test.js
@@ -16,42 +16,46 @@ var test = require('tap').test;
 var createStatemachine = require('../statemachine');
 var redisClient = kue.redis.client();
 
-function reset(callback) {
-    redisClient.keys('q:*', function(err, keys) {
-        if (err) return callback(err);
+function setup(name, _callback) {
+    test(name, function(t) {
+        var callback = function(err) {
+            if (err) {
+                t.equal(err, null, 'setup should not error');
+                return t.end();
+            };
 
-        var multi = redisClient.multi();
-        keys.forEach(function(key) {
-            multi.del(key);
+            _callback(t, createStatemachine());
+        }
+        redisClient.keys('q:*', function(err, keys) {
+            if (err) return callback(err);
+
+            var multi = redisClient.multi();
+            keys.forEach(function(key) {
+                multi.del(key);
+            });
+            multi.exec(callback);
         });
-        multi.exec(callback);
     });
 }
 
-test('a quick job should be finished properly', function(t) {
-    t.plan(3);
-    reset(function(err) {
-        t.equal(err, null, 'reset should not error');
-        if (err) return t.end();
-        var statemachine = createStatemachine();
-
-        statemachine.process('first', function(data, callback) {
-            t.ok(true, 'first job should be processed');
-            callback(null);
-        });
-        statemachine.process('second', function(data, callback) {
-            t.ok(true, 'second (last) job should be processed');
-            t.end();
-        });
-
-        statemachine.createProcedure({}, [{
-            name: 'first',
-            data: {}
-        }, {
-            name: 'second',
-            data: {}
-        }]).execute();
+setup('a quick job should be finished properly', function(t, statemachine) {
+    t.plan(2);
+    statemachine.process('first', function(data, callback) {
+        t.ok(true, 'first job should be processed');
+        callback(null);
     });
+    statemachine.process('second', function(data, callback) {
+        t.ok(true, 'second (last) job should be processed');
+        t.end();
+    });
+
+    statemachine.createProcedure({}, [{
+        name: 'first',
+        data: {}
+    }, {
+        name: 'second',
+        data: {}
+    }]).execute();
 });
 
 test('shutdown', function(t) {
